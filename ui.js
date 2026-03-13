9aba1b2a48cabc079842e82e45a5506a19e8cf50
donmoyero <63630549+donmoyero@users.noreply.github.com>
2026-03-13 10:04:47 -0700

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,17 @@
+function togglePlay(){
 
+if(!Editor.video) return;
+
+if(Editor.playing){
+
+Editor.video.pause();
+Editor.playing = false;
+
+}else{
+
+Editor.video.play();
+Editor.playing = true;
+
+}
+
+}
