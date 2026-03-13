c5a6fc68953ada6e7891043ec14d4ba24862c93b
donmoyero <63630549+donmoyero@users.noreply.github.com>
2026-03-13 10:03:40 -0700

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,1 +1,17 @@
+function loadVideo(file){
 
+const url = URL.createObjectURL(file);
+
+const vid = document.createElement("video");
+
+vid.src = url;
+vid.crossOrigin = "anonymous";
+
+vid.onloadedmetadata = () => {
+
+Editor.video = vid;
+Editor.duration = vid.duration;
+
+};
+
+}
